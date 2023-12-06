e1cb22818155f0290cb3e74543be9d5fdf873156
Eli Manzo <96manzo.eli@gmail.com>
2023-12-06 11:44:25 -0800

    Adding randomize string functionality

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+
+function randomizeString(s) {
+  const arr = [...s]
+  const randomArr = []
+  while (arr.length > 0) {
+    randomIndex = Math.floor(Math.random() * arr.length)
+    randomArr.push(arr[randomIndex])
+    arr.splice(randomIndex, 1)
+  }
+  return randomArr.join('')
+}
 
 export default function App() {
+  const letters = 'abcdefghijklmnopqrstuvwxyz'
+  const [randomLetters, setRandomLetters] = useState(null)
+
+  const getRandomLetters = () => {
+    setRandomLetters(randomizeString(letters))
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lets randomize the alphabet: {letters}</Text>
+      {randomLetters && <Text>Randomized: {randomLetters}</Text>}
+      <Button title="Randomize Me!" onPress={getRandomLetters}></Button>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +35,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
