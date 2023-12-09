237e1c229309814e17cbc9b44100d20a4e7f88c8
Eli Manzo <96manzo.eli@gmail.com>
2023-12-09 13:17:07 -0800

    feature: added - more words to guess, input validation, and styles

--- App.js
+++ App.js
@@ -3,15 +3,33 @@ import { useState } from 'react';
 
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
+const WORDBANK = [
+  'node',
+  'react',
+  'linkedlist',
+  'heap',
+  'stack',
+  'queue',
+  'trees'
+];
+
 function randomizeString(s) {
+  if (new Set(s).size <= 1) {
+    throw new Error('Invalid String: String could not be randomized');
+  }
   const arr = [...s];
   const randomArr = [];
   while (arr.length > 0) {
-    randomIndex = Math.floor(Math.random() * arr.length);
+    const randomIndex = getRandomIndex(arr);
     swapElements(arr, randomIndex, arr.length - 1);
     randomArr.push(arr.pop());
   }
-  return randomArr.join('');
+  const randomString = randomArr.join('');
+  return randomString === s ? randomizeString(s) : randomString;
+}
+
+function getRandomIndex(arr) {
+  return Math.floor(Math.random() * arr.length);
 }
 
 function swapElements(arr, i, j) {
@@ -19,15 +37,18 @@ function swapElements(arr, i, j) {
 }
 
 export default function App() {
-  const letters = 'node';
+  const [currentWord, setCurrentWord] = useState('');
   const [randomLetters, setRandomLetters] = useState('');
   const [userWord, setUserWord] = useState('');
-  const [winLoss, setWinLoss] = useState(null);
-  const updateRandomLetters = () => {
-    setRandomLetters(randomizeString(letters));
+  const [winLoss, setWinLoss] = useState('');
+  const setWordToGuess = () => {
+    setWinLoss('');
+    const word = WORDBANK[getRandomIndex(WORDBANK)].toUpperCase();
+    setCurrentWord(word);
+    setRandomLetters(randomizeString(word));
   };
   const didYouGuessIt = () => {
-    if (userWord === letters) {
+    if (userWord === currentWord) {
       setWinLoss('Won!');
     } else {
       setWinLoss('Loss!');
@@ -35,16 +56,52 @@ export default function App() {
   };
   return (
     <View style={styles.container}>
-      <Text>Press the start button to start guessing!</Text>
-      {randomLetters && <Text>Guess Me! {randomLetters}</Text>}
-      <Button title='Start' onPress={updateRandomLetters} />
-      <TextInput
-        value={userWord}
-        onChangeText={setUserWord}
-        placeholder='Type something here'
-      />
-      <Button title='Submit' onPress={didYouGuessIt} />
-      {winLoss && <Text>Status: {winLoss}</Text>}
+      <Text style={styles.title}>Word Scramble!</Text>
+      {randomLetters !== '' ? (
+        <>
+          <Text style={styles.word}>Word: {randomLetters}</Text>
+          <TextInput
+            style={styles.input}
+            value={userWord}
+            onChangeText={(text) => {
+              const setLetters = new Set(currentWord);
+              const setStr = ['[^', ...setLetters, ']+'].join('');
+              const regex = new RegExp(setStr, 'g');
+              setUserWord(text.toUpperCase().replace(regex, ''));
+            }}
+            placeholder='Enter Word Guess'
+          />
+          {winLoss === '' && (
+            <Button
+              title='Submit'
+              onPress={didYouGuessIt}
+              style={styles.button}
+            />
+          )}
+          {winLoss !== '' && (
+            <View style={styles.resultContainer}>
+              <Text style={styles.resultText}>
+                You {winLoss} The word was {currentWord}. Press the reset to
+                start again!
+              </Text>
+              <Button
+                title='Reset'
+                onPress={setWordToGuess}
+                style={styles.button}
+              />
+            </View>
+          )}
+        </>
+      ) : (
+        <>
+          <Text style={styles.instructions}>Press Start to play!</Text>
+          <Button
+            title='Start'
+            onPress={setWordToGuess}
+            style={styles.button}
+          />
+        </>
+      )}
       <StatusBar style='auto' />
     </View>
   );
@@ -56,5 +113,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20
+  },
+  word: {
+    fontSize: 18,
+    marginBottom: 10
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 8
+  },
+  button: {
+    marginTop: 10
+  },
+  resultContainer: {
+    marginTop: 20,
+    alignItems: 'center'
+  },
+  resultText: {
+    marginBottom: 10
+  },
+  instructions: {
+    fontSize: 16,
+    marginBottom: 20
   }
 });
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# word-scramble
+# Word Scramble
+
+![Word Scramble Play](https://i.gyazo.com/a766baa07465b64d95fc015d0eb50166.png)
