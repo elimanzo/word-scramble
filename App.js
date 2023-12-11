aa6fc9284db5e2f25901ac3df827fdeb74b11013
Eli Manzo <96manzo.eli@gmail.com>
2023-12-11 02:19:00 -0800

    Adding more words, gameover, score, words can only be guessed once

--- App.js
+++ App.js
@@ -6,11 +6,15 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 const WORDBANK = [
   'node',
   'react',
-  'linkedlist',
   'heap',
-  'stack',
   'queue',
+  'priorityqueue',
   'trees',
+  'binary',
+  'stack',
+  'digit',
+  'map',
+  'frequency'
 ];
 
 function randomizeString(s) {
@@ -37,72 +41,113 @@ function swapElements(arr, i, j) {
 }
 
 export default function App() {
+  const [wordBank, setWordBank] = useState([...WORDBANK]);
+  const [gameOver, setGameOver] = useState(false);
   const [currentWord, setCurrentWord] = useState('');
   const [randomLetters, setRandomLetters] = useState('');
   const [userWord, setUserWord] = useState('');
   const [winLoss, setWinLoss] = useState('');
+  const [score, setScore] = useState({ wins: 0, losses: 0 });
   const setWordToGuess = () => {
     setWinLoss('');
-    const word = WORDBANK[getRandomIndex(WORDBANK)].toUpperCase();
+    const randomIndex = getRandomIndex(wordBank);
+    const word = wordBank[randomIndex].toUpperCase();
     setCurrentWord(word);
     setRandomLetters(randomizeString(word));
+    wordBank.splice(randomIndex, 1);
+    setWordBank(wordBank);
   };
   const didYouGuessIt = () => {
     if (userWord === currentWord) {
+      setScore((prev) => ({ ...prev, wins: prev.wins + 1 }));
       setWinLoss('Won!');
     } else {
-      setWinLoss('Loss!');
+      setScore((prev) => ({ ...prev, losses: prev.losses + 1 }));
+      setWinLoss('Lost!');
     }
   };
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Word Scramble!</Text>
-      {randomLetters !== '' ? (
+      {gameOver ? (
         <>
-          <Text style={styles.word}>Word: {randomLetters}</Text>
-          <TextInput
-            style={styles.input}
-            value={userWord}
-            onChangeText={(text) => {
-              const setLetters = new Set(currentWord);
-              const setStr = ['[^', ...setLetters, ']+'].join('');
-              const regex = new RegExp(setStr, 'g');
-              setUserWord(text.toUpperCase().replace(regex, ''));
-            }}
-            placeholder="Enter Word Guess"
-          />
-          {winLoss === '' && (
-            <Button
-              title="Submit"
-              onPress={didYouGuessIt}
-              style={styles.button}
-            />
-          )}
-          {winLoss !== '' && (
-            <View style={styles.resultContainer}>
-              <Text style={styles.resultText}>
-                You {winLoss} The word was {currentWord}. Press the reset to
-                start again!
-              </Text>
-              <Button
-                title="Reset"
-                onPress={setWordToGuess}
-                style={styles.button}
-              />
-            </View>
-          )}
-        </>
-      ) : (
-        <>
-          <Text style={styles.instructions}>Press Start to play!</Text>
+          <Text style={styles.word}>GAME OVER!</Text>
           <Button
-            title="Start"
-            onPress={setWordToGuess}
+            title='Game Over'
+            onPress={() => {
+              setWordBank([...WORDBANK]);
+              setGameOver(false);
+              setUserWord('');
+              setRandomLetters('');
+              setScore({ wins: 0, losses: 0 });
+            }}
             style={styles.button}
           />
         </>
+      ) : (
+        <>
+          {randomLetters !== '' ? (
+            <>
+              <Text style={styles.word}>Word: {randomLetters}</Text>
+              <TextInput
+                style={styles.input}
+                value={userWord}
+                onChangeText={(text) => {
+                  const setLetters = new Set(currentWord);
+                  const setStr = ['[^', ...setLetters, ']+'].join('');
+                  const regex = new RegExp(setStr, 'g');
+                  setUserWord(text.toUpperCase().replace(regex, ''));
+                }}
+                placeholder='Enter Word Guess'
+              />
+              {winLoss === '' && (
+                <Button
+                  title='Submit'
+                  onPress={didYouGuessIt}
+                  style={styles.button}
+                />
+              )}
+              {winLoss !== '' && (
+                <View style={styles.resultContainer}>
+                  <Text style={styles.resultText}>
+                    You {winLoss} The word was {currentWord}. Press the reset to
+                    start again!
+                  </Text>
+                  <Text style={styles.resultText}>
+                    Won: {score.wins} Lost: {score.losses} Win Ratio:{' '}
+                    {Math.floor(
+                      (score.wins / (score.losses + score.wins)) * 100
+                    )}{' '}
+                    %
+                  </Text>
+                  <Button
+                    title='Reset'
+                    onPress={() => {
+                      if (wordBank.length > 0) {
+                        setWordToGuess();
+                        setUserWord('');
+                      } else {
+                        setGameOver(true);
+                      }
+                    }}
+                    style={styles.button}
+                  />
+                </View>
+              )}
+            </>
+          ) : (
+            <>
+              <Text style={styles.instructions}>Press Start to play!</Text>
+              <Button
+                title='Start'
+                onPress={setWordToGuess}
+                style={styles.button}
+              />
+            </>
+          )}
+        </>
       )}
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -112,35 +157,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   title: {
-    fontSize: 24,
-    marginBottom: 20,
+    fontSize: 26,
+    marginBottom: 20
   },
   word: {
-    fontSize: 18,
-    marginBottom: 10,
+    fontSize: 20,
+    marginBottom: 10
   },
   input: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 10,
-    paddingHorizontal: 8,
+    paddingHorizontal: 8
   },
   button: {
-    marginTop: 10,
+    marginTop: 10
   },
   resultContainer: {
     marginTop: 20,
-    alignItems: 'center',
+    alignItems: 'center'
   },
   resultText: {
-    marginBottom: 10,
+    fontSize: 16,
+    marginHorizontal: 15,
+    marginBottom: 10
   },
   instructions: {
-    fontSize: 16,
-    marginBottom: 20,
-  },
+    fontSize: 20,
+    marginBottom: 20
+  }
 });
